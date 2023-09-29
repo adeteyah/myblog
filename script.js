892e91afd2c967476b315e52fae94b59d5543ed5
Adeteyah <adeteyah@gmail.com>
2023-09-30 02:29:43 +0700

    5.5%

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// tilts
+VanillaTilt.init(document.querySelector(".video-source"), {
+    max: 1,
+    speed: 1200,
+    scale: 1.5,
+    "mouse-event-element":  ".video-hero h1"
+});
+
+VanillaTilt.init(document.querySelector(".video-hero h1"), {
+    max: 4,
+    speed: 1000,
+    scale: .75
+});
+
+VanillaTilt.init(document.querySelector(".nav"), {
+    max: 1,
+    speed: 1200
+});
+
+//dark mode
 const switchTheme = () => {
     const rootElem = document.documentElement
     let dataTheme = rootElem.getAttribute('data-theme'), 
